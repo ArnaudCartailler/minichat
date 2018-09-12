d68a34daa00bd913911fd5a709902c737779d670
arnaudcartailler <arnaud.cartailler@gmail.com>
2018-09-12 15:54:15 +0200

    work done

---
+++ minichat.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+
+
+      <form action="minichat_post.php" method="post">
+        <label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" placeholder="pseudo" value="" />
+        <label for="message">Message</label> : <input type="text" name="message" placeholder="message" value="" />
+
+        <input type="submit"name="valid" value="Envoyer le formulaire" />
+      </form>
+
+<?php
+
+    try{
+
+    $bdd = new PDO('mysql:host=localhost;dbname=minichat;charset=utf8', 'root', '');
+
+    }
+
+    catch(Exception $e){
+
+          die('Erreur : '.$e->getMessage());
+    }
+
+
+  $reponse = $bdd->query('SELECT pseudo, message FROM minichat ORDER BY ID DESC LIMIT 0, 10');
+
+  while ($donnees = $reponse->fetch()){
+
+  	echo '<p><strong>' . htmlspecialchars($donnees['pseudo']) . '</strong> : ' . htmlspecialchars($donnees['message']) . '</p>';
+  }
+
+  $reponse->closeCursor();
+
+  ?>
+
+  <script src="js/vendor/modernizr-3.6.0.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+  <script src="js/plugins.js"></script>
+  <script src="js/main.js"></script>
+
+  <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+  <script>
+    window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
+    ga('create', 'UA-XXXXX-Y', 'auto'); ga('send', 'pageview')
+  </script>
+  <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+</body>
+
+</html>
