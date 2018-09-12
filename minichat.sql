a7cdf72ff275c18e02bcc7939e46a997298b37cd
arnaudcartailler <arnaud.cartailler@gmail.com>
2018-09-12 16:30:12 +0200

    add bdd

---
+++ minichat.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Sep 12, 2018 at 04:24 PM
+-- Server version: 10.1.35-MariaDB
+-- PHP Version: 7.2.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `minichat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `minichat`
+--
+
+CREATE TABLE `minichat` (
+  `id` int(11) NOT NULL,
+  `pseudo` varchar(255) NOT NULL,
+  `message` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `minichat`
+--
+
+INSERT INTO `minichat` (`id`, `pseudo`, `message`) VALUES
+(1, 'Pierre', 'Le 1er à 4 a perdu ! 1 !'),
+(2, 'Paul', '2 !'),
+(3, 'Jacques', '3 !');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `minichat`
+--
+ALTER TABLE `minichat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `minichat`
+--
+ALTER TABLE `minichat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
